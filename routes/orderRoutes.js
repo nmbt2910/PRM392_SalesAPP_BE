91794ab034763abca23724aae57f82ecce6eff14
Huynh Duong <huynhtd97@gmail.com>
2025-10-26 15:40:24 +0700

    payment

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createOrder, getAllOrders, getOrderDetails, updatePaymentStatus } = require('../controllers/orderController');
-const { protect } = require('../middleware/authMiddleware');
+const {
+  createOrder,
+  getAllOrders,
+  getOrderDetails,
+  updatePaymentStatus,
+} = require("../controllers/orderController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').post(protect, createOrder);
-router.route('/getAllOrders').get(protect, getAllOrders);
-router.route('/getOrdersById/:orderID').get(protect, getOrderDetails);
-router.route('/updatePaymentStatus/:orderID').put(protect, updatePaymentStatus);
+router.route("/").post(protect, createOrder);
+router.route("/getAllOrders").get(protect, getAllOrders);
+router.route("/getOrdersById/:orderID").get(protect, getOrderDetails);
+router.route("/updatePaymentStatus/:orderID").put(protect, updatePaymentStatus);
 
 module.exports = router;
